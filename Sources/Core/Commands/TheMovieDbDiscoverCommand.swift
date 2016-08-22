24aa06c5ba77cc64fc56d2f95bddadaf0a4525f4
Alexnaldo Santos <alexnaldo@iMac-de-Alexnaldo-2.local>
2016-08-22 07:55:26 -0300

    fix: public alias

--- Sources/Core/Commands/TheMovieDbDiscoverCommand.swift
+++ Sources/Core/Commands/TheMovieDbDiscoverCommand.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftyJSON
 
-typealias TheMovieDbDiscoverListModel = TheMovieDbListModel<TheMovieDbDiscoverModel>
+public typealias TheMovieDbDiscoverListModel = TheMovieDbListModel<TheMovieDbDiscoverModel>
 
 public class TheMovieDbDiscoverModel: TheMovieDbModel {
     
